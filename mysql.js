055b8eab4563da0ac06f81291544fd83fe071d04
unknown <drewalcazar23@gmail.com>
2018-09-29 00:21:47 -0700

    fixed typo's added readme/gitignore

--- mysql.js
+++ mysql.js
@@ -1,6 +1,7 @@
 const express = require("express");
 // const mysql = require("../../../../AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/mysql");
 const app = express();
+const path = require("path");
 const port = process.env.PORT || 5000;
 // var connection = mysql.createConnection({
 //   host: "localhost",
@@ -15,7 +16,17 @@ const port = process.env.PORT || 5000;
 //     console.log("Connection Established with MySQL.");
 //   }
 // });
-app.listen(port, () => console.log(`listening on port ${port}`));
+
 app.get("/api", (req, res) => {
   res.send("Up and running!");
 });
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "src")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "src", "index.html"));
+  });
+}
+
+app.listen(port, () => console.log(`listening on port ${port}`));
