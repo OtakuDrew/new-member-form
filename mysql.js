124fa04e805c36035da4fa2616312dc236b0a933
unknown <drewalcazar23@gmail.com>
2018-10-05 14:59:25 -0700

    sigh

--- mysql.js
+++ mysql.js
@@ -76,4 +76,4 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(port, () => console.log(`listening on port ${port}`));
+app.listen(port, "0.0.0.0", () => console.log(`listening on port ${port}`));
